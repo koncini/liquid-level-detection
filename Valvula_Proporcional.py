a0e67ce1f69f47fefb95b850e2e314fe4f4af86e
Sebastian <sebastian.rincon@actum.com.co>
2022-11-17 17:00:06 -0500

    manejo_mas_detallado_de_apertura

--- Valvula_Proporcional.py
+++ Valvula_Proporcional.py
@@ -6,6 +6,7 @@ STEP = 21  #pin 40 to TB6560 clk+
 CW =1
 CCW =0
 CDIR = None
+current_status = 0
 
 gpio.setmode(gpio.BCM)
 gpio.setup(DIR, gpio.OUT)
@@ -14,42 +15,67 @@ gpio.setup(STEP, gpio.OUT)
 # Main body of code
 try:
     while True:
+        print("Valve Current Status %s '%'" % current_status)
         x = input()
         if x == "C":
-            print("Cerrando")
-            gpio.output(DIR,CCW)
-            for x in range(100):
+            print("Cerrando 100%")
+            current_status = current_status - 100
+            gpio.output(DIR,CW)
+            for x in range(800):
                 gpio.output(STEP,gpio.HIGH)
-                sleep(.0050)
+                sleep(.0005)
                 gpio.output(STEP,gpio.LOW)
-                sleep(.0050)
+                sleep(.0005)
 
         if x == "O":
-            print("Abriendo")
-            gpio.output(DIR,CW)
-            for x in range(100):
+            print("Abriendo 100%")
+            current_status = current_status + 100
+            gpio.output(DIR,CCW)
+            for x in range(800):
                 gpio.output(STEP,gpio.HIGH)
-                sleep(.0050)
+                sleep(.0005)
                 gpio.output(STEP,gpio.LOW)
-                sleep(.0050)
+                sleep(.0005)
 
-        if x == "M":
+        if x == "MC":
             print("Cerrado 50%")
-            gpio.output(DIR,CCW)
-            for x in range(50):
+            current_status = current_status - 50
+            gpio.output(DIR,CW)
+            for x in range(400):
                 gpio.output(STEP,gpio.HIGH)
-                sleep(.0050)
+                sleep(.0005)
                 gpio.output(STEP,gpio.LOW)
-                sleep(.0050)
+                sleep(.0005)
 
-        if x == "Q":
+        if x == "QC":
             print("Cerrado 25%")
-            gpio.output(DIR,CCW)
-            for x in range(22):
+            current_status = current_status - 25
+            gpio.output(DIR,CW)
+            for x in range(200):
                 gpio.output(STEP,gpio.HIGH)
-                sleep(.0050)
+                sleep(.0005)
                 gpio.output(STEP,gpio.LOW)
-                sleep(.0050)
+                sleep(.0005)
+
+        if x == "MO":
+            print("Abriendo 50%")
+            current_status = current_status + 50
+            gpio.output(DIR,CCW)
+            for x in range(400):
+                gpio.output(STEP,gpio.HIGH)
+                sleep(.0005)
+                gpio.output(STEP,gpio.LOW)
+                sleep(.0005)
+
+        if x == "QO":
+            print("Abriendo 25%")
+            current_status = current_status + 25
+            gpio.output(DIR,CCW)
+            for x in range(200):
+                gpio.output(STEP,gpio.HIGH)
+                sleep(.0005)
+                gpio.output(STEP,gpio.LOW)
+                sleep(.0005)
 
 except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
     print("Cleaning up!")
